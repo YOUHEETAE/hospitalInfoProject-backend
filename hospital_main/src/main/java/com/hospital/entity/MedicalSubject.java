195208dde8856595753de426998b9ba31ff0e422
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2025-09-02 19:53:07 +0900

    fix: Pharmacy 엔티티, ProDoc 엔티티 ToString 어노테이션 추가

--- hospital_main/src/main/java/com/hospital/entity/MedicalSubject.java
+++ hospital_main/src/main/java/com/hospital/entity/MedicalSubject.java
@@ -14,6 +14,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -21,6 +22,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = "hospital")
 @Table(name = "medical_subject")
 public class MedicalSubject {
 
