888f9cd8a1b8c5a82151e16b9af356016a9ba508
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2025-10-19 04:19:13 +0900

    Update ChatbotResponse.java

--- hospital_main/src/main/java/com/hospital/dto/ChatbotResponse.java
+++ hospital_main/src/main/java/com/hospital/dto/ChatbotResponse.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class ChatbotResponse {
     
     /**
-     * 응답 타입: question, recommendation, inappropriate
+     * 응답 타입: question, suggest, inappropriate, emergency
      */
     @JsonProperty("type")
     private String type;
@@ -31,7 +31,7 @@ public class ChatbotResponse {
     
 
     /**
-     * 여러 진료과 목록(recommendation 일 때)
+     * 여러 진료과 목록(suggest 일 때)
      */
     @JsonProperty("departments")
     private List<String> departments;
@@ -41,6 +41,9 @@ public class ChatbotResponse {
      */
     public boolean isQuestion() {
         return "question".equals(type);
+    }
+    public boolean isEmergency() {
+    	return "emergency".equals(type);
     }
 
     public boolean isRecommendation() {
